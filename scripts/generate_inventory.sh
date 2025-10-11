423365dc5c5d14cef9ceb9aec01e086e1a97a4dd
Hrubech HOMBESSA <hrubech@vmi2251464.contaboserver.net>
2025-10-12 00:03:31 +0200

    update deploy script

--- scripts/generate_inventory.sh
+++ scripts/generate_inventory.sh
@@ -9,7 +9,7 @@ WEB_IP=$(terraform output -raw web_public_ip)
 
 cat > "$INVENTORY_FILE" <<EOF
 [webservers]
-webserver ansible_host=${WEB_IP} ansible_user=ubuntu ansible_ssh_private_key_file=${SSH_KEY_PATH}
+webserver ansible_host=${WEB_IP} ansible_user=ec2-user ansible_ssh_private_key_file=${SSH_KEY_PATH}
 
 
 [all:vars]
