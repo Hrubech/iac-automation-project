f7be937e6c3216099b9598bf4a6ab8a9de98d797
Hrubech HOMBESSA <hrubech@vmi2251464.contaboserver.net>
2025-10-29 15:55:37 +0100

    add ansible role

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "automation" {
-    key_name = var.key_name
-    public_key = file(var.ssh_public_key_path)
+  key_name   = var.key_name
+  public_key = file(var.ssh_public_key_path)
 }
 
 # ------------------------------------
@@ -43,12 +43,14 @@ resource "aws_security_group" "webserver_sg" {
 }
 
 resource "aws_instance" "webserver" {
-    ami = var.ami_id
-    instance_type = var.instance_type
-    key_name = aws_key_pair.automation.key_name
-    vpc_security_group_ids = [aws_security_group.webserver_sg.id]
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = aws_key_pair.automation.key_name
+  vpc_security_group_ids = [aws_security_group.webserver_sg.id]
 
-    tags = {
-        Name = "iac-webserver"
-    }
+  tags = {
+    Name    = "webserver"
+    Project = "iac-webserver"
+    Role    = "apache"
+  }
 }
